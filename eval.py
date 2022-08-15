2450d2b88891d15f3005004eb74771ec60feadf8
Jonathan Becktor <jonathanBecktor@gmail.com>
2022-08-15 12:58:48 +0200

    Letterbox class

--- eval.py
+++ eval.py
@@ -8,7 +8,7 @@ import timm
 import torch
 import ttach as tta
 from torch.utils.data import Dataset, DataLoader
-from DataLoader import ShippingLabClassification, letterbox
+from DataLoader import ShippingLabClassification, Letterbox
 from torchvision import transforms, utils
 import matplotlib.pyplot as plt
 from tqdm import tqdm
